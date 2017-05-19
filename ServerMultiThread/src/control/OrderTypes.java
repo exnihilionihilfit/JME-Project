c2cd3b8126fc336a784a1079aef08b9f9b5d6014
Antarius <Antarius@users.noreply.github.com>
2017-05-19 16:43:27 +0200

    commited the rest of the files i missed the last time

---
+++ ServerMultiThread/src/control/OrderTypes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import com.jme3.network.serializing.Serializable;
+
+/**
+ * all different Types of Orders a Player can execute
+ * default is stay Order, doing nothing
+ * @author Antarius
+ */
+public enum OrderTypes {
+
+    NOT_DEFINED,
+    STAY,
+    MOVE,
+    MINE,
+    MOVETOOBJECT;
+}
